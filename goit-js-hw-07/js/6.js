aace95440e3c81e1c13886a89952404b08d48585
Ior Sapko <galvatron260781@gmail.com>
2020-07-05 23:11:12 +0300

    second commit of hw07 (task06 fixed)

--- goit-js-hw-07/js/6.js
+++ goit-js-hw-07/js/6.js
@@ -3,9 +3,11 @@ const input = document.querySelector("#validation-input");
 const checkOfQuantityOfInputedSymbols = function () {
   if (input.value.length === 6) {
     input.classList.replace("invalid", "valid");
-    console.log(input.value.length);
+    input.classList.add("valid");
+    // console.log(input.value.length);
   } else {
-    input.classList.toggle("invalid");
+    input.classList.replace("valid", "invalid");
+    input.classList.add("invalid");
   }
 };
 input.addEventListener("blur", checkOfQuantityOfInputedSymbols);
